caec611361913aaba0359afdc86d2454466e5b03
rujool11 <132924872+rujool11@users.noreply.github.com>
2024-03-13 20:37:44 +0530

    13/3/24, findPages in allocate_books updated to use res variable

--- Binary_Search/BS_on_answers/allocate_books.cpp
+++ Binary_Search/BS_on_answers/allocate_books.cpp
@@ -34,3 +34,26 @@ int findPages(vector<int>& arr, int n, int m) {
 
     return low;
 }
+
+int findPages(vector<int>& arr, int n, int m) {
+    int res = -1;
+
+    if (m > n) return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+
+        if (students <= m) {
+            res = mid;  // Update res if the current allocation is valid
+            high = mid - 1;  // Explore lower allocation
+        } else {
+            low = mid + 1;  // Explore higher allocation
+        }
+    }
+
+    return res;
+}
+// ^ using res 

dd4cca3d793ebb5d321f5db743a8eff5fa4b0fa5
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-03-25 20:16:01 +0530

    add proper comments for frog_jump_II

--- DP/one_D/frog_jump_II.cpp
+++ DP/one_D/frog_jump_II.cpp
@@ -7,24 +7,25 @@
 // Determine the minimum total cost required for the frog to reach the top.
 
 class Solution {
-  public:
+    public:
     
-    int f(int ind, vector<int>& heights, vector<int>& dp) {
-        
-        if (ind == 0) return 0;
-        if (dp[ind] != -1) return dp[ind];
-        
-        int a = f(ind-1, heights, dp) + abs(heights[ind] - heights[ind-1]);
-        int b = INT_MAX;
-        if (ind > 1)  b = f(ind-2, heights, dp) + abs(heights[ind] - heights[ind-2]);
-        return dp[ind] = min(a, b);
-    }
-    
-    
-    int minCost(vector<int>& height) {
-        
-        int n = height.size();
-        vector<int> dp(n + 1, -1);
-        return f(n-1, height, dp);
-    }
-};
+      int f(int n, vector<int>& height, vector<int>& dp) {
+          if (n==0) return 0; // convert to 0 based indexing so 0 to n-1
+          
+          if (dp[n] != -1) return dp[n];
+          int a = f(n-1, height, dp) + abs(height[n] - height[n-1]);
+          int b = INT_MAX; // initialize as INT_MAX for the case when n > 1
+          // if it could have come from n-2, i.e n >= 2, calculate for that
+          if (n > 1) b = f(n-2, height, dp) + abs(height[n] - height[n-2]);
+          return dp[n] = min(a, b);
+      }
+      
+      int minCost(vector<int>& height) {
+          int n = height.size();
+          vector<int> dp(n+1, -1);
+          return f(n-1, height, dp);
+      }
+  };
+
+// space optimized ->
+

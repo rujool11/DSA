9d1de82a3c6865127f4aad2c57f866a5575ec93b
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-04-09 23:17:46 +0530

    add proper mergeSort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,40 +1,48 @@
-void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; 
-    int left = low;      
-    int right = mid + 1;   
-
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
+class Solution {
+    public:
+        void merge(vector<int>& nums, int low, int mid, int high) {
+            vector<int> temp;
+            int left = low;
+            int right = mid+1;
+    
+            while (left<=mid && right<=high) {
+                if (nums[left] < nums[right]) {
+                    temp.push_back(nums[left]);
+                    left++;
+                } 
+                else {
+                    temp.push_back(nums[right]);
+                    right++;
+                }
+            }
+    
+            while (left <= mid) {
+                temp.push_back(nums[left]);
+                left++;
+            }
+    
+            while (right <= high) {
+                temp.push_back(nums[right]);
+                right++;
+            }
+    
+            for (int i=low; i<=high; i++) {
+                nums[i] = temp[i-low];
+            }
         }
-        else {
-            temp.push_back(arr[right]);
-            right++;
+    
+        void ms(vector<int>& nums, int low, int high) {
+            if (low>=high) return;
+            int mid = (low + high)/2;
+    
+            ms(nums, low, mid);
+            ms(nums, mid+1, high);
+            merge(nums, low, mid, high);
         }
-    }
-
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
-
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
-}
-
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  
-    mergeSort(arr, mid + 1, high); 
-    merge(arr, low, mid, high);  
-}
+    
+        vector<int> mergeSort(vector<int>& nums) {
+            ms(nums, 0, nums.size()-1);
+            return nums;
+        }
+    };
+    
